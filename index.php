6c97163b056c8c08fc2963941d1e3eeb7b573801
David Gruian <david.gruian@gmail.com>
2020-12-07 15:03:56 +0200

    added jquery

--- index.php
+++ index.php
@@ -6,9 +6,14 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Management Energetic</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 <body>
-It Works
 
+<script>
+    $(document).ready(function (){
+
+    })
+</script>
 </body>
 </html>
