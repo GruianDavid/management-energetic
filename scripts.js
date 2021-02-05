a4e6dd50428910823807f5a600b79978303ea3ba
David Gruian <david.gruian@gmail.com>
2021-02-05 11:56:45 +0200

    added methods for population of tables

--- scripts.js
+++ scripts.js
@@ -12,14 +12,43 @@ $(document).ready(function (){
     let Rsh = 1000
 
     let valueToBeRead = 0.4
-    // getWantedValueFor(0.4)
+    let backupFunctionsTable ='';
 
-    for (let voltage = 1; voltage<20; voltage++){
+    for (let voltage = 2; voltage<20; voltage++){
+        resetFunctionsTable()
         let current = getWantedValueFor(voltage/20)
         if (current < 0){
+            //if current is negative load the last functions table
+            $('.form-output-functions .table-body').empty().append(backupFunctionsTable)
             break
+        }else{
+            setValuesInProcessedTable(voltage,current)
         }
-        console.log('Voltage: '+voltage/20+", Current: "+current+", Power: "+current*(voltage/20)+", Rezistenta: "+(voltage/20)/current)
+    }
+
+    function setValuesInProcessedTable(voltage,current){
+        $('.form-output-processed .table-body').append('' +
+            '<ul class="table-row">' +
+            '<li class="cell" contenteditable>'+(voltage/20).toFixed(2)+'</li>' +
+            '<li class="cell" contenteditable>'+(current).toFixed(8)+'</li>' +
+            '<li class="cell" contenteditable>'+(current*(voltage/20)).toFixed(8)+'</li>' +
+            '<li class="cell" contenteditable>'+((voltage/20)/current).toFixed(8)+'</li>' +
+            '</ul>')
+    }
+
+    function setValuesInFunctionsTable(fx,f1x,xn1){
+        $('.form-output-functions .table-body').append('' +
+            '<ul class="table-row">' +
+            '<li class="cell">'+(fx).toFixed(8)+'</li>' +
+            '<li class="cell">'+(f1x).toFixed(8)+'</li>' +
+            '<li class="cell">'+(xn1).toFixed(8)+'</li>' +
+            '</ul>')
+    }
+
+    function resetFunctionsTable(){
+        let table = $('.form-output-functions .table-body')
+        backupFunctionsTable = table.clone()
+        table.empty()
     }
 
     function getWantedValueFor(valueToBeRead){
@@ -32,6 +61,7 @@ $(document).ready(function (){
             index ++;
             fx = (isc+ki*(t-273))*Ir/Im-I0*(Math.exp(q*(valueToBeRead+Rs*xn1)/n/k/t)-1)-(valueToBeRead+Rs*xn1)/Rsh-xn1
             f1x = -I0*q*Rs*Math.exp(q*(valueToBeRead+xn1*Rs)/k/n/t)/k/n/t-Rs/Rsh-1
+            setValuesInFunctionsTable(fx,f1x,xn1)
             xn1 = xn1 - fx/f1x
             if (xn1.toFixed(14) === lastValue.toFixed(14)){
                 consecutiveSameResults++
