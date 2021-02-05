6fa9136092ca0f9d91aef4905b7e46b90b9efb79
David Gruian <david.gruian@gmail.com>
2021-02-05 12:55:37 +0200

    added chart js

--- scripts.js
+++ scripts.js
@@ -79,4 +79,44 @@ $(document).ready(function (){
         }
     }
 
+
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'scatter',
+        data: {
+            datasets: [
+                {
+                    label: 'Putere',
+                    data: [{x: 1, y: 2}, {x: 2, y: 4}, {x: 3, y: 8},{x: 4, y: 16}],
+                    showLine: true,
+                    fill: false,
+                    borderColor: 'rgba(0, 200, 0, 1)'
+                },
+                {
+                    label: 'Curent',
+                    data: [{x: 1, y: 3}, {x: 3, y: 4}, {x: 4, y: 6}, {x: 6, y: 9}],
+                    showLine: true,
+                    fill: false,
+                    borderColor: 'rgba(200, 0, 0, 1)'
+                }
+            ]
+        },
+        options: {
+            tooltips: {
+                mode: 'index',
+                intersect: false,
+            },
+            hover: {
+                mode: 'nearest',
+                intersect: true
+            },
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            },
+        }
+    });
 })
